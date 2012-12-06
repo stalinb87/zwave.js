dd22d32a9c2f96c9e858956cbab2f8ed5ab27c78
stalin <stalin@satellite.(none)>
2012-12-05 21:54:39 -0400

    Los primeros archivos

---
+++ TestZwave.js
@@ -0,0 +1,29 @@
+var Zwave = require('./lib/Zwave.js');
+function main(){
+	var zwave = new Zwave();
+	zwave.open("/dev/ttyUSB0",false,function(){
+	var encender =false;
+	/*setInterval(function(){
+		if(encender){
+			zwave.swichOn(2);
+		} else {
+			zwave.swichOff(2);
+		}
+		encender=!encender;
+	},500);*/
+	
+		zwave.swichOn(2);
+		/*zwave.swichOff(2);
+		zwave.swichOn(2);
+		zwave.swichOff(2);
+		zwave.swichOn(2);
+		zwave.swichOff(2);
+		zwave.swichOn(2);
+		zwave.swichOff(2);
+		zwave.swichOn(2);
+		zwave.swichOff(2);
+		zwave.swichOn(2);
+*/
+});
+}
+main();
